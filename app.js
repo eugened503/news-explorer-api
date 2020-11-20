139572e544e41486d87259d2b858c5ca04057eb2
Eugen Larionov <zhenia.larionoff2015@yandex.ru>
2020-11-20 22:34:09 +0300

    fixing errors cors

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
@@ -10,24 +9,13 @@ const rateLimit = require('express-rate-limit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const NotFoundError = require('./errors/not-found-err');
-
-const allowedAddress = ['https://api.news-line.students.nomoreparties.xyz', 'http://api.news-line.students.nomoreparties.xyz'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedAddress.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
+const allowedCors = [
+  'https://api.news-line.students.nomoreparties.xyz',
+  'http://api.news-line.students.nomoreparties.xyz',
+  'localhost:3000'
+];
 const { PORT = 3000 } = process.env;
 const app = express();
-
-app.use(cors(corsOptions));
-
 app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/newsdb', {
@@ -37,10 +25,6 @@ mongoose.connect('mongodb://localhost:27017/newsdb', {
   useUnifiedTopology: true,
 });
 
-
-
-
-
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 минут
   max: 100, // допустимый лимит: 100 запросов с одного IP
@@ -49,10 +33,16 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(requestLogger);
-// подключаем логгер запросов
+app.use(requestLogger); // подключаем логгер запросов
 
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
 
+  if (allowedCors.includes(origin)) { // Проверяем, что значение origin есть среди разрешённых доменов
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 app.use('/', router);
 app.use(errorLogger); // подключаем логгер ошибок
